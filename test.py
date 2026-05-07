79d572eea80e6ee1bed1a699f48cf698dbeaae43
Ayush3513 <166975215+Ayush3513@users.noreply.github.com>
2026-05-07 06:18:02 +0000

    model changed2

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 def action_confirm(self):
  for rec in self:
-  orders=self.env['sale.order'].   search([('partner_id','=',rec.partner_id.id)])
+  orders=self.env['sale.order'].      search([('partner_id','=',rec.partner_id.id)])
   for order in orders:
    if order.state==  "  draft"    :     
-    order.write({'note':   'confirmed'})
+    order.write({'note'   :   'confirmed'})
     print(order.name)
