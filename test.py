29f9b33862c7268501d38594f913ca6d71d0620b
Ayush3513 <166975215+Ayush3513@users.noreply.github.com>
2026-05-07 06:55:18 +0000

    advanced features4

--- test.py
+++ test.py
@@ -1,7 +1,31 @@
 def action_confirm(self):
- for rec in self:
-  orders=self.env['sale.order'].      search([('partner_id','=',rec.partner_id.id)])
-  for order in orders:
-   if order.state==  "  draft"    :     
-    order.write({'note'   :   'confirmed'})
-    print(order.name)
+
+    for rec in self:
+
+        sale_orders = self.env['sale.order'].search([
+            ('partner_id', '=', rec.partner_id.id)
+        ])
+
+        for order in sale_orders:
+
+            if order.state == "draft":
+
+                if order.amount_total > 0:
+
+                    order.write({
+                        'note': 'order confrimed succesfully'
+                    })
+
+                    print("Order Confirmed => ", order.name)
+
+                    self.env.cr.execute("""
+                        UPDATE sale_order
+                        SET client_order_ref='done'
+                        WHERE id=%s
+                    """ % order.id)
+
+                else:
+                    pass
+
+            else:
+                print("already confirmed")
